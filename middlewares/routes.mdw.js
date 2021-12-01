0091229ce0a416a7fecabc4aa4c7dcd02ccfbaa4
Giang <gianghh2001@gmail.com>
2021-12-02 01:34:44 +0700

    Update mobile and laptop hbs

--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -5,20 +5,42 @@ import productmodel from "../models/productmodel.js";
 const __dirname=dirname(fileURLToPath(import.meta.url))
 import userRoute from '../routes/indexrouter.js'
 import userModel from "../models/usermodel.js";
+import router from "../routes/indexrouter.js";
 //routes
 export default function(app) {
     app.get('/', async function (req, res)  {
-        const list=await productmodel.findTop5ProHighest();
-        const test=await productmodel.find();
-        console.log(new Date(test[0].end).getTime());
+        const highestprice=await productmodel.findTop5ProHighest();
+        const mostbids=await productmodel.findTop5ProMostBids();
+        const instance=await productmodel.findTop5ProInstance();
        res.render('index',{
-           products:list,
-           empty:list.length===0,
+           highestprice,
+           mostbids,
+           instance,
+           empty:mostbids.length===0 || highestprice.length===0 ,
        });
     })
 
+    app.get('/product/mobilephone',async function (req, res) {
+        const list_1 = await productmodel.findMobile();
+        console.log(new Date(list_1[0].end).getTime());
+        res.render('productType/mobilephone', {
+            products: list_1,
+            empty: list_1.length === 0,
+        });
+    });
+    app.get('/product/laptop',async function (req, res) {
+        const list_2 = await productmodel.findLaptop();
+        console.log(new Date(list_2[0].end).getTime());
+        res.render('productType/laptop', {
+            products: list_2,
+            empty: list_2.length === 0,
+        });
+    });
     app.use('/user',userRoute);
+=======
 
+    app.use('/',userRoute);
+>>>>>>> 39b70c0a3dc74a9a231ec8fb3efbb8f89c56bce8
 
     app.use(function (req,res)
     {
--- models/productmodel.js
+++ models/productmodel.js
@@ -6,6 +6,23 @@ export default{
     findTop5ProHighest(){
         return db('product').limit(5).offset(0).orderBy('current','DESC');
     },
+    findAll() {
+        return db('product');
+    },
+    findMobile() {
+        return db('product').where('category', 1);
+    },
+    findLaptop() {
+        return db('product').where('category', 2);;
+=======
+    findTop5ProMostBids(){
+        return db('product').limit(5).offset(0).orderBy('bids','DESC');
+    }
+    ,
+    findTop5ProInstance(){
+        return db('product').limit(5).offset(0).orderBy('cap','DESC');
+>>>>>>> 39b70c0a3dc74a9a231ec8fb3efbb8f89c56bce8
+    },
     async find(){
         const ob= await db('product').where('id',1);
         return ob;
