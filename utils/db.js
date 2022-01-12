0b0d8636158a6ed8ce2c6f3425aeb5e1ffc5cc81
hoangphuc552001@gmail.com <hoangphuc552001@gmail.com>
2022-01-12 21:54:16 +0700

    updata db

--- utils/db.js
+++ utils/db.js
@@ -2,11 +2,11 @@ import fn from "knex";
 const knex = fn({
     client: 'mysql2',
     connection: {
-        host : '127.0.0.1',
-        port : 3306,
-        user : 'root',
-        password : '',
-        database : 'auction'
+        host: 'us-cdbr-east-05.cleardb.net',
+        port: 3306,
+        user: 'be44caddfce4b6',
+        password: '00bbb032',
+        database: 'heroku_7067838459c52ba'
     },
     pool: { min: 0, max: 10 },
 });
