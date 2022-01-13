59e4c202356683f9cc34ce884cfa86c6ba21a643
phamngocanh19127098 <npham4533@gmail.com>
2022-01-13 20:53:42 +0700

    change admin middleware

--- middlewares/admin.mdw.js
+++ middlewares/admin.mdw.js
@@ -1,5 +1,9 @@
 export default function validAdmin(req,res,next){
     if(req.session.admin===false){
+        
+        if(req.session.authenticated===true){
+            return res.redirect('/');
+        }
         req.session.retUrl= req.originalUrl;
         return res.redirect('/login');
     }
