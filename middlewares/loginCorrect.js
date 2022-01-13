a1d263154ba38814025be0e1b768bdfc1a6afa44
phamngocanh19127098 <npham4533@gmail.com>
2022-01-13 13:16:57 +0700

    update middleware handle otp

--- middlewares/loginCorrect.js
+++ middlewares/loginCorrect.js
@@ -2,10 +2,11 @@ export default function validLogin (req,res,next){
     if(typeof (req.session.logined)==='undefined'){
         req.session.logined=false;
     }
-    if(req.session.logined===false){
+
+    if(req.session.logined===false||req.session.authenticated===true){
         req.session.retUrl= req.originalUrl;
-        return res.redirect('/login');
+        return res.redirect('/');
     }
-    
+
     next();
 }
